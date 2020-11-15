d03990f89e9291ec4ef82e22c99c1938885f380e
Lee Seong Won <syanggu@naver.com>
2020-11-15 14:51:08 +0900

    File fix

--- file.c
+++ file.c
@@ -12,32 +12,32 @@ int main(void)
 
   // 파일에 쓰기
 
-  // FILE * file = fopen("c:\\test1.txt", "wb"); // \\ 두개써야 하나로 인식 // r w a 읽기 쓰기 이어쓰기 // t b 텍스트 바이널데이터
-  // if (file == NULL)
-  // {
-  //   printf("파일 열기 실패\n");
-  //   return 1;
-  // }
-  // fputs("fputs 를 이용해서 글을 적어볼게요\n", file);
-  // fputs("잘 적히는지 확인해주세요\n", file);
+  FILE * file = fopen("c:\\test1.txt", "wb"); // \\ 두개써야 하나로 인식 // r w a 읽기 쓰기 이어쓰기 // t b 텍스트 바이널데이터
+  if (file == NULL)
+  {
+    printf("파일 열기 실패\n");
+    return 1;
+  }
+  fputs("fputs 를 이용해서 글을 적어볼게요\n", file);
+  fputs("잘 적히는지 확인해주세요\n", file);
 
   // 파일 읽기
 
-  // FILE * file = fopen("c:\\test1.txt", "rb"); // \\ 두개써야 하나로 인식 // r w a 읽기 쓰기 이어쓰기 // t b 텍스트 바이널데이터
-  // if (file == NULL)
-  // {
-  //   printf("파일 열기 실패\n");
-  //   return 1;
-  // }
+  FILE * file = fopen("c:\\test1.txt", "rb"); // \\ 두개써야 하나로 인식 // r w a 읽기 쓰기 이어쓰기 // t b 텍스트 바이널데이터
+  if (file == NULL)
+  {
+    printf("파일 열기 실패\n");
+    return 1;
+  }
   
-  // while (fgets(line, MAX, file) != NULL)
-  // {
-  //   printf("%s", line);
-  // }
+  while (fgets(line, MAX, file) != NULL)
+  {
+    printf("%s", line);
+  }
 
   // 파일을 열고 닫지 않은 상태에서 어떤 프로그램에 문제가 생기면 ?
   // 데이터 손실 발생 가능 ! 그래서 항상 파일을 닫아주는 습관을 들어주세요
-  // fclose(file);
+  fclose(file);
 
   // fprintf, fscanf 쌍
 
@@ -48,16 +48,16 @@ int main(void)
 
   // 파일 쓰기
 
-  // FILE * file = fopen("c:\\test2.txt", "wb");
-  // if (file == NULL)
-  // {
-  //   printf("파일 열기 실패");
-  //   return 1;
-  // }
+  FILE * file = fopen("c:\\test2.txt", "wb");
+  if (file == NULL)
+  {
+    printf("파일 열기 실패");
+    return 1;
+  }
 
-  // // 로또 추첨 번호 저장
-  // fprintf(file, "%s %d %d %d %d %d %d\n", "추첨번호", 1, 2, 3, 4, 5, 6);
-  // fprintf(file, "%s %d\n", "보너스번호", 7);
+  // 로또 추첨 번호 저장
+  fprintf(file, "%s %d %d %d %d %d %d\n", "추첨번호", 1, 2, 3, 4, 5, 6);
+  fprintf(file, "%s %d\n", "보너스번호", 7);
 
   // 파일 읽기
 
